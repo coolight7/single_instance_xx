dca7169c1f759ceeaeec4071250911fd971c30c2
coolight <2465045051@qq.com>
2024-12-17 01:03:27 +0800

    add example

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <single_instance_xx/single_instance_xx_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  SingleInstanceXxPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SingleInstanceXxPlugin"));
+}
